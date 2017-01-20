6b5f1dc1b56993ad3e8126436caabbd9dab761b2
christopher-lamkin <cjlamkin64@gmail.com>
2017-01-20 09:04:16 -0800

    log different value types and add an example of the delete operator

--- javascript/basics/javascript-operators.js
+++ javascript/basics/javascript-operators.js
@@ -59,3 +59,24 @@ console.log('5 << 1:', (5 << 1));
 console.log('5 >> 1:', (5 >> 1));
 
 // typeof vs instanceof
+
+var num = 1;
+var price = 1.5;
+var name = 'Chris';
+var trueValue = true;
+var nullVar = null
+var und;
+
+console.log('typeof num:', typeof num);
+console.log('typeof price:', typeof price);
+console.log('typeof name:', typeof name);
+console.log('typeof trueValue:', typeof trueValue);
+console.log('typeof nullVar:', typeof nullVar);
+console.log('typeof und:', typeof und);
+console.log('typeof [1,2,3]:', typeof [1,2,3]);
+
+// Delete Operators
+
+var myObj = {name: 'John', age:21};
+delete myObj.age;
+console.log(myObj);
