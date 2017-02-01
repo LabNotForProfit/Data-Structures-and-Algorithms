f952c8fea4ddd57f0dca5a6815c72100ce1fffdf
christopher-lamkin <cjlamkin64@gmail.com>
2017-02-01 11:48:35 -0800

    create method for removing node at position

--- javascript/basics/linked-lists.js
+++ javascript/basics/linked-lists.js
@@ -52,7 +52,28 @@ function LinkedList() {
   };
 
   this.insert = function(position, element){};
-  this.removeAt = function(position){};
+
+  this.removeAt = function(position){
+    if (position > -1 && position < length){
+      var current = head,
+          previous,
+          index = 0;
+      if (position === 0){
+        head = current.next;
+      } else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+        previous.next = current.next;
+      }
+      length--;
+      return current.element;
+    } else {
+      return null;
+    }
+  };
+  
   this.remove = function(element){};
   this.indexOf = function(element){};
   this.isEmpty = function(){};
