fad3aa12412a6e1ec008d9f9b816b5bec808a4d4
christopher-lamkin <cjlamkin64@gmail.com>
2017-02-02 11:59:20 -0800

    add insert method for doubly linked list

--- javascript/basics/doubly-linked-lists.js
+++ javascript/basics/doubly-linked-lists.js
@@ -16,5 +16,46 @@ function DoublyLinkedList(){
   var head = null,
       tail = null,
       length = 0;
-  
+
+  this.insert(element, position){
+    if (position >= 0 && position <= length){
+      var node = new Node(element),
+          current = head,
+          previous,
+          index = 0;
+
+      if (position === 0){
+        if (!head) {
+          head = node;
+          tail = node;
+        } else {
+          node.next = current;
+          current.prev = node;
+          head = node;
+        }
+      } else if (position === length) {
+          current = tail;
+          current.next = node;
+          node.prev = current;
+          tail = node;
+      } else {
+        while (index++ < position){
+          previous = current;
+          current = current.next;
+        }
+        node.next = current;
+        previous.next = node;
+
+        current.prev = node;
+        node.prev = previous;
+      }
+      length++;
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+
+
 }
